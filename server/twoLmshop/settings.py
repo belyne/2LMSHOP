db25e3e112e6177bfac5e858d78694cc4daccead
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-12-19 19:48:17 +0100

    added files necessary for deployment

--- server/twoLmshop/settings.py
+++ server/twoLmshop/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".vercel.app"]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
